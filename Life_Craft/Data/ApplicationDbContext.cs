b1d27ec551b75f1190bacdb78f7006000e3f257f
harris <harriskwong120898@gmail.com>
2024-01-22 22:15:23 -0500

    Added migration to database (Category model)

---
+++ Life_Craft/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Life_Craft.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Life_Craft.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
