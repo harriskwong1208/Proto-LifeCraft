2802c98192af3b45ff5ec80bf02881ba0b9854e3
harris <harriskwong120898@gmail.com>
2024-01-22 22:27:45 -0500

    Changed DisplayOrder from string to int type, seeded Category table with some data inside DbContext file

--- Life_Craft/Migrations/20240123032546_changed DisplayOrder to Int Type.Designer.cs
+++ Life_Craft/Migrations/20240123032546_changed DisplayOrder to Int Type.Designer.cs
@@ -3,6 +3,7 @@ using Life_Craft.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Life_Craft.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240123032546_changed DisplayOrder to Int Type")]
+    partial class changedDisplayOrdertoIntType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
