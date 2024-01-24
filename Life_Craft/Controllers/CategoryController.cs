cf2368799fdf78646b18a14140ffb3d51e95d39c
harris <harriskwong120898@gmail.com>
2024-01-24 18:05:21 -0500

    Added create action in category controller

--- Life_Craft/Controllers/CategoryController.cs
+++ Life_Craft/Controllers/CategoryController.cs
@@ -1,10 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Life_Craft.Data;
+using Life_Craft.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Life_Craft.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) { 
+            _db = db;
+        }
         public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
         {
             return View();
         }
