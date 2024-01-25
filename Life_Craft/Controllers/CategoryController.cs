5db9b0487c079f9be8d04f1d880a310c4b2eba1a
harris <harriskwong120898@gmail.com>
2024-01-24 19:40:10 -0500

    Created create http post method in category controller.

--- Life_Craft/Controllers/CategoryController.cs
+++ Life_Craft/Controllers/CategoryController.cs
@@ -19,5 +19,16 @@ namespace Life_Craft.Controllers
         {
             return View();
         }
-    }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+            /*If want to redirect to different controller, put controller name as second parameter */
+			//return RedirectToAction("Index","Home");
+
+		}
+	}
 }
