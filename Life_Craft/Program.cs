7764587f63b9cbe8305ee523b734ffefc3932c03
harris <harriskwong120898@gmail.com>
2024-02-07 20:28:59 -0500

    Added scaffolded item (identity).

--- Life_Craft/Program.cs
+++ Life_Craft/Program.cs
@@ -2,6 +2,7 @@ using LifeCraft.DataAccess.Data;
 using LifeCraft.DataAccess.Repository;
 using LifeCraft.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options=>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 //Added, so when we want the implementation in the category controller class, it will know that it will have the immplementation of CategoryRepository
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
