abb7062c3c4f8ec7df5479119d32fdc48323c21e
harris <harriskwong120898@gmail.com>
2024-02-07 20:48:02 -0500

    Added service and app mapping in program.cs because identity uses razor pages instead of mvc

--- Life_Craft/Program.cs
+++ Life_Craft/Program.cs
@@ -17,6 +17,9 @@ builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<App
 //Added, so when we want the implementation in the category controller class, it will know that it will have the immplementation of CategoryRepository
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
+//Added because identity uses razor pages instead mvc 
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -34,7 +37,8 @@ app.UseRouting();
 //Added this middleware for authentication, must be added before authorization
 app.UseAuthentication();
 app.UseAuthorization();
-
+//Added for the identity razor pages
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
