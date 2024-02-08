d808c809be102e3a6202017ac8f31f7b54614784
harris <harriskwong120898@gmail.com>
2024-02-07 20:58:01 -0500

    Added migration and updated database, this automatically created identity related tables in database because we added the identity scaffolded item.

--- LifeCraft.DataAccess/Migrations/20240208015325_Add identity Table.Designer.cs
+++ LifeCraft.DataAccess/Migrations/20240208015325_Add identity Table.Designer.cs
@@ -4,16 +4,19 @@ using LifeCraft.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace LifeCraft.DataAccess.Data.Migrations
+namespace LifeCraft.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240208015325_Add identity Table")]
+    partial class AddidentityTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
